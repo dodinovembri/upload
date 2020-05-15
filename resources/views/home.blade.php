bf550d5a72da11035acefb1a7666aa33a693e267
dodi <dodinovembri32@gmail.com>
2020-05-16 02:29:51 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    @if(Session::has('alert-success'))
+                        <div class="alert alert-success">
+                            <strong>{{ \Illuminate\Support\Facades\Session::get('alert-success') }}</strong>
+                        </div>
+                    @endif
+                    <form method="POST" action="{{ route('upload') }}" enctype="multipart/form-data">
+                        @csrf
+                        <input type="file" name="file">
+                        <br><br>
+                        <input type="submit" value="Upload">                    
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
